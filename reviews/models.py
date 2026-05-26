3d17119feb30d265749931bfca5e55a07347ca6a
mrsmlisa <lisa.q.scott@gmail.com>
2026-05-26 09:04:20 +0200

    adding model in review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True)
+    status = models.IntegerField(choices=[(0, 'Draft'), (1, 'Published')], default=0)
+    excerpt = models.CharField(max_length=255, blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
