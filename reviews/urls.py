eae3cc88c4783f807bfee2a9c0849b970e893c37
mrsmlisa <lisa.q.scott@gmail.com>
2026-05-27 11:39:00 +0200

    new migration. changes in navbar html

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,6 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
-    path('review/<slug:slug>/', views.review_detail, name='review_detail'),
-    path('', views.index, name='index'),
+    path('review/<slug:slug>/', views.ReviewDetailView.as_view(), name='review_detail'),
+    path('', views.ReviewListView.as_view(), name='index'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Review
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'reviews/index.html')
+class ReviewListView(ListView):
+    model = Review
+    template_name = 'reviews/index.html'
+    context_object_name = 'reviews'
+    queryset = Review.objects.filter(status=1).order_by('-created_at')
+    paginate_by = 6
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = 'reviews/review_detail.html'
+    context_object_name = 'review'
+    slug_url_kwarg = 'slug'
 
 def review_detail(request, slug):
     return render(request, 'reviews/review_detail.html', {'slug': slug})
