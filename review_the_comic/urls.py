715a1a153c6a2da0a2382c1577705f7ba7439edf
mrsmlisa <lisa.q.scott@gmail.com>
2026-05-28 13:05:00 +0200

    had used the django autofill to much and had to fix some things

--- review_the_comic/urls.py
+++ review_the_comic/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import views
+
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from reviews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,7 +28,3 @@ urlpatterns = [
     path('review/<slug:slug>/', views.ReviewDetailView.as_view(), name='review_detail'),
     path('comment/<slug:slug>/', views.CommentListView.as_view(), name='comment_list'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
