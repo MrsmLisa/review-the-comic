bed0a24c25f3ce276b226dc3a3b19bb71783f17e
mrsmlisa <lisa.q.scott@gmail.com>
2026-06-10 10:39:41 +0200

    Added that image on review is mandatory and did a new test on forsm.py

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,13 +6,25 @@ class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = Review
-        fields = ['title', 'featured_image',
-                  'book_author', 'content', 'excerpt'
-                  ]
+        fields = [
+            "title",
+            "featured_image",
+            "book_author",
+            "content",
+            "excerpt",
+        ]
+
+    def featured_image(self):
+        image = self.cleaned_data.get("featured_image")
+        if not image:
+            raise forms.ValidationError(
+                "Please upload a cover image for your review."
+            )
+        return image
 
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ['content']
+        fields = ["content"]
